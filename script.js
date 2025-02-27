f51e04bada2021485f916b98e3837e5960da2819
Aman Goswami <148433162+Amangoswami01@users.noreply.github.com>
2025-02-27 20:25:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function changeMessage() {
 function toggleTheme() {
     document.body.classList.toggle("dark-mode");
 }
+// Scroll Animation Function
+function revealOnScroll() {
+    let elements = document.querySelectorAll(".fade-in");
+    elements.forEach((el) => {
+        let windowHeight = window.innerHeight;
+        let elementTop = el.getBoundingClientRect().top;
+        if (elementTop < windowHeight - 50) {
+            el.classList.add("visible");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
