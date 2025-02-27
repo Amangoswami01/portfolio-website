7863eb1e8d1aa7a5b7624af3aa88e7d74d4a21ec
Aman Goswami <148433162+Amangoswami01@users.noreply.github.com>
2025-02-27 19:50:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-// Button click pe message change karne ka function
+// Button click pe text change karne ka function
 function changeMessage() {
     document.getElementById("message").innerText = "Thanks for visiting my portfolio!";
+}
+
+// Theme change karne ka function
+function toggleTheme() {
+    document.body.classList.toggle("dark-mode");
 }
